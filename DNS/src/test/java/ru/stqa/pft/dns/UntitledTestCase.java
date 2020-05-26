746234ec0547ef8feeb372111a6fa13e0688caab
Squzet-Kaledo <64698160+Squzet-Kaledo@users.noreply.github.com>
2020-05-26 10:19:04 +0300

    Тесты

---
+++ DNS/src/test/java/ru/stqa/pft/dns/UntitledTestCase.java
@@ -0,0 +1,110 @@
+package com.example.tests;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import ru.stqa.pft.dns.SeleniumAssertion;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.testng.Assert.fail;
+
+public class UntitledTestCase {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+
+  @BeforeClass(alwaysRun = true)
+  public void setUp() throws Exception {
+    driver = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
+    baseUrl = "https://www.google.com/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    driver.manage().window().maximize();
+  }
+
+  @Test
+  public void testAssertion() {
+    driver.get("https://www.dns-shop.ru/catalog/");
+    WebElement element1 = driver.findElement(By.cssSelector(".catalog-spoiler"));
+    new Actions(driver)
+            .moveToElement(element1,160,5)
+            .click()
+            .perform();
+    WebElement element2 = driver.findElement(By.linkText("Компьютеры"));
+    new Actions(driver)
+    .moveToElement(element2)
+            .perform();
+    WebElement element3 = driver.findElement(By.linkText("Комплектующие для ПК"));
+    new Actions(driver)
+            .moveToElement(element3)
+            .click()
+            .perform();
+    driver.findElement(By.linkText("Процессоры")).click();
+    driver.findElement(By.cssSelector(".ui-checkbox-dropdown__label")).click();
+    SeleniumAssertion check = new SeleniumAssertion(driver);
+    boolean a = check.assertPresentElementLocated(By.cssSelector(".product-pricei__current"));
+    boolean b = check.assertPresentElementLocated(By.cssSelector(".product-priceop__current"));
+    SoftAssert s = new SoftAssert();
+    s.assertEquals(a,true);
+    s.assertEquals(b, true);
+    s.assertAll();
+
+    /*SoftAssert a = new SoftAssert();
+    a.assertEquals(By.cssSelector(".product-price__current"));
+
+    a.assertAll();
+   /* assertFalse(isElementPresent(By.cssSelector("label:nth-child(2) span")));*/
+
+  }
+
+
+
+          @AfterClass(alwaysRun = true)
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
